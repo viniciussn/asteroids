b15a6407bb3148cefdb7dacc082008e35aa39a99
viniciussn <vinicius_snogueira@hotmail.com>
2019-11-08 23:43:40 -0300

    updates

--- main.js
+++ main.js
@@ -5,51 +5,60 @@ window.onload = function() {
         init: game_init,
         tick: game_tick
     });
-    /* ============================================================================================================= */
-    /* criando a cena e adicionando o componente game_engine*/
+     /* ============================================================================================================= */
+    /* variáveis globais */    
     var scene = document.createElement('a-scene');
+    var camera = document.createElement('a-camera');
+    var ceu = document.createElement('a-sky');
+
+    var asteroids = [];
+    var qt_asteroids = 100;
+    var asteroid_speed = 0.5;
+    var asteroid_radius = 1;
+    var asteroid_min_origem = -100;
+    var asteroid_max_origem = 100;
+    var asteroid_min_initial_distance = 50;
+
+    /* ============================================================================================================= */
+    /* criando a cena e adicionando o componente game_engine*/    
     document.body.appendChild(scene);
     var entity = document.createElement('a-entity');
     entity.setAttribute('game_engine', '');
     scene.appendChild(entity);
     /* ============================================================================================================= */
-    /* configurando a câmera */    
-    var camera = document.createElement('a-camera');
+    /* configurando a câmera */        
     camera.setAttribute('position', '0 0 0');
     camera.setAttribute('wasd-controls-enabled', 'false');
     scene.appendChild(camera);
     /* ============================================================================================================= */
-    /* criando céu */    
-    var ceu = document.createElement('a-sky');
+    /* criando céu */        
     ceu.setAttribute('src', 'https://images.unsplash.com/photo-1475274047050-1d0c0975c63e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80');
     ceu.setAttribute('rotation', '0 -90 0');
     scene.appendChild(ceu);
-    /* ============================================================================================================= */
-
-    /* variáveis globais */    
-    var asteroids = [];
-    var qt_asteroids = 10;
-    var asteroid_speed = 0.5;
-    var asteroid_radius;
+   
 
     
     function game_init(){
-        generate_asteroids(scene, 10, 0, 10000);
+        generate_asteroids();
     }
     
     function game_tick(){
         for(var i=0; i<qt_asteroids; i++){
-            asteroids[i].object3D.lookAt(0, 0, 0);
             asteroids[i].object3D.translateZ(asteroid_speed);
+            console.log(asteroids[0].object3D.position);
         }
     }
 
-    function generate_asteroids(scene, max, min){
+    function generate_asteroids(){
         for(var i=0; i<qt_asteroids; i++){
             var sphere = document.createElement('a-sphere');
-            var x = 10*i, y = 0, z = -50;
+            var x = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
+            var y = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
+            var z = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
 
-            sphere.setAttribute('position', x+' '+y+' '+z);
+            sphere.object3D.position.set(x, y, z);
+            sphere.object3D.lookAt(0, 0, 0);
+            sphere.object3D.translateZ(-asteroid_min_initial_distance);
             sphere.setAttribute('radius', asteroid_radius);
             sphere.setAttribute('color', '#'+new THREE.Color(Math.random(), Math.random(), Math.random()).getHexString());
             scene.appendChild(sphere);
