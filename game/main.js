51891f84a1c0ad727611939d96bd92944200ce79
viniciussn <vinicius_snogueira@hotmail.com>
2019-12-03 21:35:48 -0300

    correções

--- game/main.js
+++ game/main.js
@@ -11,29 +11,31 @@ window.onload = function() {
     var camera = document.createElement('a-camera');
     var ceu = document.createElement('a-sky');
 
-    var asteroids = [];
     var lasers = [];
+    var laser_speed = 10;
+
+    var asteroids = [];
     var qt_asteroids = 100;
-    var asteroid_speed = 0.1;
-    var laser_speed = 1;
+    var asteroid_radius = 5;
+    var asteroid_speed = 0.5;
     var asteroid_min_origem = -100;
     var asteroid_max_origem = 100;
-    var asteroid_min_initial_distance = 20;
-    var collisions = 0;
+    var asteroid_min_initial_distance = 200;
 
+    var collisions = 0;
 
     var y_height = 90;
     var z_height = -150;
-
 
     var running = false;
 
     var asteroid_obj;
     var nave;
 
-    var score,initial_msg;
+    var score;
+    var initial_msg;
 
-
+    
     /* ============================================================================================================= */
     /* criando a cena e adicionando o componente game_engine*/    
     document.body.appendChild(scene);
@@ -44,6 +46,7 @@ window.onload = function() {
     /* configurando a câmera */        
     camera.setAttribute('position', '0 0 0');
     camera.setAttribute('look-controls', 'pointerLockEnabled: true;'); 
+    camera.setAttribute('wasd-controls', 'acceleration: 1000;'); 
     // camera.setAttribute('wasd-controls-enabled', 'false');
     scene.appendChild(camera);
     /* ============================================================================================================= */
@@ -62,33 +65,29 @@ window.onload = function() {
         console.log('Carregando texturas...');    
         load_materials().then(function(){
             console.log('Texturas carregadas!');    
-
             console.log('Criando objetos...');    
             generate_objects();
             console.log('Objetos criados!');
-            
-            generate_target();
-            console.log('Mira criada');
-
-            change_score();
-            console.log('Textos score criado');
-
-            create_lives();
-            console.log('Criou as vidas');
-
-
-            initial_text();
-
-            	
         });
+        
+        generate_target();
+        console.log('Mira criada');
 
+        update_score();
+        console.log('Textos score criado');
+
+        create_lives();
+        console.log('Criou as vidas');
+
+        initial_text();
+        
     }
     
     function game_tick(){
         if(running){
             for(var i = 0; i < qt_asteroids; i++){
                 asteroids[i].translateZ(asteroid_speed);
-                detect_collision(i);
+                asteroid_laser_collision(i);
             }
             for(var i = 0; i < lasers.length; i++){
                 lasers[i].object3D.translateZ(laser_speed);
@@ -100,13 +99,13 @@ window.onload = function() {
 
     	var loader = new THREE.TextureLoader();
     	loader.setCrossOrigin('');
-		var material = new THREE.MeshLambertMaterial({
+
+        var material = new THREE.MeshLambertMaterial({
 		  map: loader.load('https://upload.wikimedia.org/wikipedia/commons/9/99/Star_icon_stylized.svg')
 		});
-		var geometry = new THREE.PlaneGeometry(10, 10*.75, {
+
+        var geometry = new THREE.PlaneGeometry(10, 10*.75, {
 			size:40,
-
-
 		});
 
 		var s1 = new THREE.Mesh(geometry, material);
@@ -128,85 +127,67 @@ window.onload = function() {
 
     function initial_text(){
 
-
     	var loader = new THREE.FontLoader();
-
 		loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-
 			var textGeo = new THREE.TextBufferGeometry( "Welcome to the Asteroids\n            Press Enter!", {
-						font: font,
-						size: 10,
-						height: 5,
-						curveSegments: 1.2,
-						bevelThickness: 0.2,
-						bevelSize: 0.5,
-						bevelEnabled: true
-			} );
-
-				textGeo.computeBoundingBox();
-				var centerOffset = - 0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
-				var textMaterial = new THREE.MeshPhongMaterial( { color: 0xfff000, specular: 0xffffff } );
-				initial_msg = new THREE.Mesh( textGeo, textMaterial );
-				initial_msg.position.x = -80;//centerOffset;
-				initial_msg.position.y = 20;//-250 + 67;
-				initial_msg.position.z = -100;
-				initial_msg.castShadow = true;
-				initial_msg.receiveShadow = true;
-				scene.object3D.add( initial_msg );
-				//camera.object3D.add( score );
-				
-
+                font: font,
+                size: 10,
+                height: 5,
+                curveSegments: 1.2,
+                bevelThickness: 0.2,
+                bevelSize: 0.5,
+                bevelEnabled: true
+			});
+            textGeo.computeBoundingBox();
+            var textMaterial = new THREE.MeshPhongMaterial( { color: 0xfff000, specular: 0xffffff } );
+            initial_msg = new THREE.Mesh( textGeo, textMaterial );
+            initial_msg.position.x = -80;
+            initial_msg.position.y = 20;
+            initial_msg.position.z = -100;
+            initial_msg.castShadow = true;
+            initial_msg.receiveShadow = true;
+            scene.object3D.add( initial_msg );
 		} );
-
-
     }
 
 
-    function change_score(){
+    function update_score(){
 
     	camera.object3D.remove(score);
     	var loader = new THREE.FontLoader();
 
 		loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-
 			var textGeo = new THREE.TextBufferGeometry( "Score: "+collisions, {
-						font: font,
-						size: 10,
-						height: 0.5,
-						curveSegments: 0.5,
-						bevelThickness: 0.2,
-						bevelSize: 0.1,
-						bevelEnabled: true
+                font: font,
+                size: 10,
+                height: 0.5,
+                curveSegments: 0.5,
+                bevelThickness: 0.2,
+                bevelSize: 0.1,
+                bevelEnabled: true
 			} );
-
-				textGeo.computeBoundingBox();
-				var centerOffset = - 0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
-				var textMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000, specular: 0xffffff } );
-				score = new THREE.Mesh( textGeo, textMaterial );
-				score.position.x = 140;//centerOffset;
-				score.position.y = y_height;//-250 + 67;
-				score.position.z = z_height;
-				score.castShadow = true;
-				score.receiveShadow = true;
-				camera.object3D.add( score );
-				//camera.object3D.add( score );
-				
-
+            textGeo.computeBoundingBox();
+            var textMaterial = new THREE.MeshPhongMaterial( { color: 0xff0000, specular: 0xffffff } );
+            score = new THREE.Mesh( textGeo, textMaterial );
+            score.position.x = 140;//centerOffset;
+            score.position.y = y_height;//-250 + 67;
+            score.position.z = z_height;
+            score.castShadow = true;
+            score.receiveShadow = true;
+            camera.object3D.add( score );
 		} );
-
-
     }
 
-    function detect_collision(asteroid){
+    function asteroid_laser_collision(asteroid){
     	var distance;
     	for(var j = 0; j < lasers.length; j++){
     		distance = Math.sqrt(Math.pow(asteroids[asteroid].position.x-lasers[j].object3D.position.x,2)+Math.pow(asteroids[asteroid].position.y-lasers[j].object3D.position.y,2)
     		+Math.pow(asteroids[asteroid].position.z-lasers[j].object3D.position.z,2));
-    		if(distance<1 && asteroids[asteroid].visible == true){
+    		if(distance < asteroid_radius && asteroids[asteroid].visible == true){
     			console.log("Colidiu");
     			collisions++;
-    			change_score();
-    			asteroids[asteroid].visible = false;
+    			update_score();
+                posionar_asteroid(asteroids[asteroid]);
     		}
     	}
     }
@@ -255,19 +236,13 @@ window.onload = function() {
 
     function generate_objects(){
         /* ASTEROIDS */
-        for(var i=0; i<qt_asteroids; i++){
-            var asteroid = asteroid_obj.clone();
-            var x = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
-            var y = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
-            var z = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
-
-
-            asteroid.position.set(x, y, z);
-            asteroid.lookAt(0, 0, 0);
-            asteroid.translateZ(-asteroid_min_initial_distance);
-            scene.object3D.add(asteroid);
-            asteroids.push(asteroid);
-        } 
+        // for(var i=0; i<qt_asteroids; i++){
+        //     var asteroid = asteroid_obj.clone();
+        //     posionar_asteroid(asteroid);
+        //     asteroid.scale.set(asteroid_radius, asteroid_radius, asteroid_radius);
+        //     scene.object3D.add(asteroid);
+        //     asteroids.push(asteroid);
+        // } 
 
 
         /* NAVE */
@@ -278,19 +253,33 @@ window.onload = function() {
        
     }
 
+    function posionar_asteroid(asteroid){
+        var x = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
+        var y = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
+        var z = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
+        
+        asteroid.position.set(x, y, z);
+        asteroid.lookAt(nave.position.x, nave.position.y, nave.position.z);
+        
+        asteroid.translateZ(-asteroid_min_initial_distance);
+    }
+
     function generate_stars(){
         for(var i=0; i<5000; i++){
             var sphere = document.createElement('a-sphere');
-            var x = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
-            var y = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
-            var z = Math.random() * (asteroid_max_origem - asteroid_min_origem) + asteroid_min_origem;
-
+            var x = Math.random() * 10000 - 5000;
+            var y = Math.random() * 10000 - 5000;
+            var z = Math.random() * 100000 -50000;
+            console.log(x);
+            if(z<1000 && z>0)
+                z=5000;
+            else if (z<0 && z>-1000) z=-5000;
             sphere.object3D.position.set(x, y, z);
             sphere.object3D.lookAt(0, 0, 0);
-            sphere.object3D.translateZ(-100);
-            sphere.setAttribute('radius', 0.1);
+            
+            sphere.setAttribute('radius', 4);
             sphere.setAttribute('color', '#ffffff');
-            scene.appendChild(sphere);
+            camera.appendChild(sphere);
             
         }
     }
